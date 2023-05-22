20a38908a78eea11fc9bd683b6368948419cb4a8
Jake Tamayo <hellojtmatthew@icloud.com>
2023-05-22 13:30:10 -0500

    Initial Commit

---
+++ AdditionTutor/AdditionTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdditionTutorApp.swift
+//  AdditionTutor
+//
+//  Created by Jacob Tamayo on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct AdditionTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
