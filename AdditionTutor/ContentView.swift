bb8f5bedfe267cf3404a448964ca446728d4f1ce
Jake Tamayo <hellojtmatthew@icloud.com>
2023-05-22 14:22:20 -0500

    feat: Add an array of Emojis as an @State property
    These emojis are used to show the numbers that are randomly generated when the screen first appears. The number of emojis is used as a visualization for the math problem the user needs to solve

--- AdditionTutor/ContentView.swift
+++ AdditionTutor/ContentView.swift
@@ -10,10 +10,23 @@ import SwiftUI
 struct ContentView: View {
     @State private var firstNumber = 0
     @State private var secondNumber = 0
+    @State private var firstNumberEmojis = "" // holds the first number of randomly selected emojis
+    @State private var secondNumberEmojis = "" // // holds the second number of randomly selected emojis
+
     private var emojis = ["🍕", "🍎", "🍏", "🐵", "👽", "🧠", "🧜🏽‍♀️", "🧙🏿‍♂️", "🥷", "🐶", "🐹", "🐣", "🦄", "🐝", "🦉", "🦋", "🦖", "🐙", "🦞", "🐟", "🦔", "🐲", "🌻", "🌍", "🌈", "🍔", "🌮", "🍦", "🍩", "🍪"]
+    
 
     var body: some View {
         VStack {
+            Group {
+                Text("\(firstNumberEmojis)")
+                Text("+")
+                Text("\(secondNumberEmojis)")
+            }
+            .font(.system(size: 80))
+            
+            Spacer()
+            
             HStack {
                 Text("\(firstNumber)")
                 Text("+")
@@ -24,11 +37,23 @@ struct ContentView: View {
         .padding()
         // When the app loads, set firstNumber and secondNumber to random ints
         .onAppear {
+            // firstNumber and secondNumber is used to generate a random number from 1 to 10
+            //  - these numbers will be used to assign a random repeated emoji
             firstNumber = Int.random(in: 1...10)
             secondNumber = Int.random(in: 1...10)
-            
-
+            firstNumberEmojis = randomEmojiGenerator(number: firstNumber)
+            secondNumberEmojis = randomEmojiGenerator(number: secondNumber)
         }
+    }
+    
+    // This function needs to return a string becuase it will be used with firstNumberEmojis and secondNumberEmojis which are strings
+    func randomEmojiGenerator(number: Int) -> String {
+        var emojiString = ""
+        let randomEmoji = emojis.randomElement()!
+        for _ in 1...number {
+            emojiString.append(randomEmoji)
+        }
+        return emojiString
     }
 }
 
